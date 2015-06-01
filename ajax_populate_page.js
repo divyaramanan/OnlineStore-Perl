b54be6106f413528ab884280208330faf6551fb7
divyaramanan <divyaramanan.r@gmail.com>
2015-06-01 12:02:39 -0700

    Update ajax_populate_page.js

--- ajax_populate_page.js
+++ ajax_populate_page.js
@@ -1,13 +1,7 @@
 /*  We load the global array proj3_data once, then use it as needed
     to retrieve product information.
     
-    The Milk Chocolate handler is done the old-fashion way, with an 
-    'onclick' call in the xhtml code.  The rest of the buttons have 
-    handlers assigned the correct way.
-    
-    Alan Riggins
-    CS545
-    Fall 2013
+  
 */    
 
 
